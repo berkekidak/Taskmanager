a9327922e0e100ea3b07a14be58e2fda0df40c1f
Alireza <alireza2000jadidi@gmail.com>
2026-09-01 14:38:24 +0200

    added basic task display

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import TaskCard from "@/components/TaskCard.vue";
+import type { Task } from "@/type/Types.ts";
+
+const task = ref<Task>({
+  id: 1,
+  title: "Plan meals",
+  done: true,
+});
+
+const newTask = ref("");
+
+function addTask() {
+  if (!newTask.value.trim()) return;
+  console.log(newTask.value);
+  newTask.value = "";
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main class="flex-center">
+    <section class="section tasks">
+      <div class="task-card">
+        <div class="task-title">
+          <img src="" alt="" />
+          <div class="task-header">
+            <h2>Mini Tasks</h2>
+            <p>simple task tracker</p>
+          </div>
+        </div>
+
+        <form @submit.prevent="addTask"></form>
+
+        <TaskCard :task="task" />
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/TaskCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { Task } from "@/type/Types.ts";
+
+defineProps<{ task: Task }>();
+</script>
+
+<template>
+  <div class="task">
+    <input type="checkbox" v-model="task.done" />
+    <p>{{ task.title }}</p>
+    <div class="task-icons">
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
