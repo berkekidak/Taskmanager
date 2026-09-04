173e8994920a4c17350f34fc4a2080ac2693083f
Berke <berke.kidak@proton.me>
2026-09-04 16:38:23 +0200

    added basic database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,55 @@
+import sqlite3
+import json
+
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 
+def create_database():
+    connection = sqlite3.connect("tasks.db")
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS tasks(
+            id INTEGER NOT NULL PRIMARY KEY,
+            title TEXT,
+            done BOOLEAN
+        )
+        """
+    )
+
+    connection.commit()
+    connection.close()
+
+
+
+def init_db():
+    connection = sqlite3.connect("tasks.db")
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        INSERT INTO tasks (id, title, done) 
+        VALUES
+        (1, "become a poet", False),
+        (2, "get groceries", True)
+        """
+    )
+
+
+    connection.commit()
+    connection.close()
+
+
+
+
+
 app = FastAPI()
+create_database()
+
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -21,20 +68,29 @@ def root():
 
 @app.get("/tasks")
 def get_all_tasks():
+    conn = sqlite3.connect("tasks.db")
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+
+    cursor.execute("SELECT * FROM tasks")
+    rows = cursor.fetchall()
+    print(rows)
+
+    tasks = []
+
+    for row in rows:
+        tasks.append({
+            "id": row["id"],
+            "title": row["title"],
+            "done": bool(row["done"])
+        })
+
+    conn.close()
+
     return {
-    "tasks": [
-        {
-        "id": 1,
-        "title": "become a poet",
-        "done": True
-        },
-        {
-        "id": 2,
-        "title": "get groceries",
-        "done": False
-        }
-    ]
-        }
-    
+        "tasks": tasks
+    }
+
 
 
